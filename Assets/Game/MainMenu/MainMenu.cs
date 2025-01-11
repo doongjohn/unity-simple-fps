c084c970f14cf5912ca75e0e67580d0732030cb8
doongjohn <doongjohn@gmail.com>
2025-01-11 21:19:50 +0900

    first commit

---
+++ Assets/Game/MainMenu/MainMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MainMenu : MonoBehaviour
+{
+    private UIDocument _document;
+    private VisualElement _root;
+    private Button _startGameButton;
+    private Button _optionsButton;
+    private Button _exitGameButton;
+
+    private void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+        _root = _document.rootVisualElement;
+
+        _startGameButton = _root.Q("StartGameButton") as Button;
+        _optionsButton = _root.Q("OptionsButton") as Button;
+        _exitGameButton = _root.Q("ExitGameButton") as Button;
+
+        _startGameButton.RegisterCallback<ClickEvent>(OnClickStartGameButton);
+        _optionsButton.RegisterCallback<ClickEvent>(OnClickOptionsButton);
+        _exitGameButton.RegisterCallback<ClickEvent>(OnClickExitGameButton);
+    }
+
+    private void OnClickStartGameButton(ClickEvent evt)
+    {
+        Debug.Log("StartGameButton");
+        SceneManager.LoadScene(Scenes.TestMap);
+    }
+
+    private void OnClickOptionsButton(ClickEvent evt)
+    {
+        Debug.Log("OptionsButton");
+    }
+
+    private void OnClickExitGameButton(ClickEvent evt)
+    {
+        Debug.Log("ExitGameButton");
+        Application.Quit();
+    }
+}
---
+++ Assets/Static/Scenes.cs
@@ -0,0 +1,5 @@
+public static class Scenes
+{
+    public static readonly string MainMenu = "Game/MainMenu/MainMenu";
+    public static readonly string TestMap = "Game/Maps/TestMap/TestMap";
+}
