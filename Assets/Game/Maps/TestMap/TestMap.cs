a27bda507a716986a42ee86a3154db79aa4371e7
doongjohn <doongjohn@gmail.com>
2025-01-12 16:23:13 +0900

    Add log

--- Assets/Game/Maps/TestMap/TestMap.cs
+++ Assets/Game/Maps/TestMap/TestMap.cs
@@ -8,13 +8,13 @@ public class TestMap : NetworkBehaviour
 
     private void Start()
     {
+        Debug.Log($"OwnerClientId: {OwnerClientId}");
         SpawnPlayerRpc(OwnerClientId);
     }
 
     [Rpc(SendTo.Server)]
     private void SpawnPlayerRpc(ulong ownerId)
     {
-        Debug.Log(ownerId);
         var player = Instantiate(PlayerPrefab);
         var network_player = player.GetComponent<NetworkObject>();
         network_player.SpawnWithOwnership(ownerId);
