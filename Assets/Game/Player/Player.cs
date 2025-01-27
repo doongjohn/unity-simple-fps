b0b17ad8e64430a3df2232562ba8d6d8b52d9cae
doongjohn <doongjohn@gmail.com>
2025-01-28 00:13:57 +0900

    Add HealthPickup

--- Assets/Game/Player/Player.cs
+++ Assets/Game/Player/Player.cs
@@ -114,6 +114,14 @@ public class Player : NetworkBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            other.GetComponent<Pickup>().OnPickup(this);
+        }
+    }
+
     private void CameraLook()
     {
         var cameraRotation = _cmFirstPersonCamera.transform.eulerAngles;
